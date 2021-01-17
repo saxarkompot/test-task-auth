5fee16c370bc779a23fd15c076ec4cab7051b84a
SaxarKompot <saxarkompot@gmail.com>
2021-01-17 22:23:06 +0100

    Create routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import Auth from './containers/Auth';
 
 class App extends Component {
    render() {
       return (
-         <div className="App">
-            <Auth />
-         </div>
+         <BrowserRouter>
+            <div className="App">
+               <Auth />
+            </div>
+         </BrowserRouter>
       );
    }
 }
--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
 import './Auth.css';
 import Login from '../Components/Login/Login';
 import Registration from '../Components/Registration/Registration';
@@ -10,9 +12,18 @@ class Auth extends Component {
    render() {
       return (
          <div className='Auth'>
-            <Login />
-            <Registration />
-            <Cv />
+            <header>
+               <nav>
+                  <ul>
+                     <li><Link to='/' exact>Login</Link></li>
+                     <li><Link to='/registration'>Registration</Link></li>
+                     <li><Link to='/cv'>Cv</Link></li>
+                  </ul>
+               </nav>
+            </header>
+            <Route path='/' exact component={Login} />
+            <Route path='/registration' component={Registration} />
+            <Route path='/cv' exact component={Cv} />
          </div>
       )
    }
